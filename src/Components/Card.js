290c922437f62eafb97e2bb4615884fb2650e7dc
vincentarisa <vincentarisa1993@gmail.com>
2022-04-06 10:56:29 +0300

    Updated the card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function Card(props){
+export default function Card(data){
     return(
         <div className="d-flex flex-row justify-content-center container-fluid ps-5 pt-5 pb-5 mb-3">
             <div className="card custom-card">
-                <img src={props.img} className="card-img-top custom-card-img" alt="Experience Image"/>
+                <img src={data.img} className="card-img-top custom-card-img" alt="Experience Image"/>
                 <div className="card-body custom-card-body">
                     <div className="d-flex">
                         <span><i className="bi bi-star-fill"/> &nbsp;</span>
-                        <span>{props.rating} <span className="grey">({props.totalrating}) .</span>&nbsp;</span>
-                        <span className="grey">{props.country}</span>
+                        <span>{data.rating} <span className="grey">({data.totalrating}) .</span>&nbsp;</span>
+                        <span className="grey">{data.country}</span>
                     </div>
-                    <p className="pt-2">{props.description}</p>
-                    <p><span className="fw-bold">From {props.price}</span> / Person</p>
+                    <p className="pt-2">{data.description}</p>
+                    <p><span className="fw-bold">From {data.price}</span> / Person</p>
                 </div>
             </div>
         </div>
